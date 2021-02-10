3d1310036a5f7d1858491982b16a07a9556f4f73
Dex Fitch <fitch.dexter@gmail.com>
2021-02-10 15:51:29 -0800

    commit tree f18e0a1a232ccb734043898338579883b15a9c23

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  decrement = () => {
+    let newSecondsLeft = this.state.secondsLeft - 1
+    this.setState({
+      secondsLeft: newSecondsLeft
+    }) 
+  }
+
+  render () {
+    let boom = this.state.secondsLeft == 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`
+    return (
+      <div onClick={this.decrement}>
+        <p>{ boom }</p>
+      </div>
+    )
+  }
+}
